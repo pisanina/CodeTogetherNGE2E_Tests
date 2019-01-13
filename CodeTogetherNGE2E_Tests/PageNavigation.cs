8d07f24e7e69bde22d09ff2c314d9cb7bb33be1a
Pisanina <mailszarookiego@o2.pl>
2019-01-13 14:31:45 +0100

    Task16: Create a first selenium webdriver test for the project

---
+++ CodeTogetherNGE2E_Tests/PageNavigation.cs
@@ -0,0 +1,55 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+
+namespace CodeTogetherNGE2E_Tests
+{
+    public class UnitTest1
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void SeleniumSetup()
+        {
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _driver.Url = "https://localhost:44362/";
+            _driver.FindElement(By.XPath("//*[@id=\"cookieConsent\"]/div/div[2]/div/button")).Click();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var Home = _driver.FindElement(By.Id("HomeTitle"));
+            Assert.True(Home.Text == ("This side is under construction."));
+
+            _driver.FindElement(By.Id("About")).Click();
+            Assert.NotNull(_driver.FindElement(By.Id("AboutPage")));
+
+            _driver.FindElement(By.Id("Contact")).Click();
+            Assert.NotNull(_driver.FindElement(By.Id("ContactPage")));
+
+            _driver.FindElement(By.Id("AddProject")).Click();
+            Assert.NotNull(_driver.FindElement(By.Id("Title")));
+
+            _driver.FindElement(By.Id("ProjectsGrid")).Click();
+            Assert.NotNull(_driver.FindElement(By.Id("ProjectGridPage")));
+
+            _driver.FindElement(By.Id("Register")).Click();
+            Assert.NotNull(_driver.FindElement(By.Id("Input_ConfirmPassword")));
+
+            _driver.FindElement(By.Id("Login")).Click();
+            Assert.NotNull(_driver.FindElement(By.Id("Input_RememberMe")));
+
+
+        }
+
+        [TearDown]
+        public void DownSelenium()
+        {
+            _driver.Quit();
+            
+        }
+    }
+}
