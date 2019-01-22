b4c9356db5826a6511fb8c08db014801a6064ee8
Pisanina <mailszarookiego@o2.pl>
2019-01-22 12:48:21 +0100

    Task35: Cover filtering feature with Selenium WebDriver tests

---
+++ CodeTogetherNGE2E_Tests/GridPage_tests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Data.SqlClient;
+using System.IO;
+using System.Reflection;
+
+namespace CodeTogetherNGE2E_Tests
+{
+    class GridPage_tests
+    {
+        private IWebDriver _driver;
+      
+
+        [TestCase("Project")] // Search in Title
+        [TestCase("Yes")]  // Search in Description
+        [TestCase("ć")]   // Test for foreign languages
+
+        public void SearchProject(string ToSearch)
+        {
+            _driver.FindElement(By.Id("ProjectsGrid")).Click();
+
+            Assert.True(_driver.PageSource.Contains("Funny"));
+            Assert.True(_driver.PageSource.Contains(ToSearch));
+
+            _driver.FindElement(By.Id("SearchInput")).SendKeys(ToSearch);
+            _driver.FindElement(By.Id("SearchButton")).Click();
+
+            Assert.False(_driver.PageSource.Contains("Funny"));
+            Assert.True(_driver.PageSource.Contains(ToSearch));
+        }
+
+
+
+
+        [SetUp]
+        public void SeleniumSetup()
+        {
+            _driver = new ChromeDriver(Configuration.WebDriverLocation);
+
+            _driver.Url = Configuration.WebApiUrl;
+
+           // Add = new AddProject_TestsPageObject(_driver);
+
+            _driver.FindElement(By.XPath("//*[@id=\"cookieConsent\"]/div/div[2]/div/button")).Click();
+        }
+
+        [TearDown]
+        public void DownSelenium()
+        {
+            _driver.Quit();
+        }
+
+    }
+}
