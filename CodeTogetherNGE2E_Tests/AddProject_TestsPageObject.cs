db17d2ea9f951fddade870fff7f2f207f177507d
Pisanina <mailszarookiego@o2.pl>
2019-02-26 20:48:22 +0100

    Task75: Project should hold reference to its owner

--- CodeTogetherNGE2E_Tests/AddProject_TestsPageObject.cs
+++ CodeTogetherNGE2E_Tests/AddProject_TestsPageObject.cs
@@ -15,6 +15,7 @@ namespace CodeTogetherNGE2E_Tests
 
         public void AddProject(string title, string description, IEnumerable<int> techIdList)
         {
+            LoginUser();
             _driver.FindElement(By.Id("AddProject")).Click();
             _driver.FindElement(By.Id("Title")).SendKeys(title);
             _driver.FindElement(By.Id("Description")).SendKeys(description);
@@ -26,6 +27,14 @@ namespace CodeTogetherNGE2E_Tests
             }
 
             _driver.FindElement(By.Id("Create")).Click();
+        }
+
+        public void LoginUser()
+        {
+            _driver.FindElement(By.Id("Login")).Click();
+            _driver.FindElement(By.Id("Input_Email")).SendKeys("TestUser@a.com");
+            _driver.FindElement(By.Id("Input_Password")).SendKeys("Qwedsa11!");
+            _driver.FindElement(By.Id("Input_Password")).SendKeys(Keys.Enter);
         }
 
         public void AddProject(string title, string description)
@@ -40,16 +49,29 @@ namespace CodeTogetherNGE2E_Tests
                 new SqlConnection(Configuration.ConnectionString))
             {
                 SQLConnect.Open();
-                using (SqlCommand Delete = new SqlCommand("Delete From ProjectTechnology Where ProjectId > 0 " +
-                    "Delete From Project Where ID > 0 DBCC CHECKIDENT('Project', RESEED, 0) " +
-                    "Insert Into Project(Title, Description) " +
-                    "Values('FirstProject', 'Our first project will be SUPRISE Hello World')," +
-                    "('SecondProject', 'Yes we will print Hello Kity')," +
-                    "('Funny bunny', 'We want to create web aplication with many funny bunes')," +
-                    "(N'Polish title like pamięć', N'Test for polish worlds like pamięć')," +
-                    "('Test for adding Project with four Tech', 'Test for adding Project with four Technologies')," +
-                    "('Project with Two Tech', 'Project with Two Tech (Test)') " +
-                    "Insert Into ProjectTechnology Values(5,2),(5, 4),(5, 6),(5, 7),(6, 6),(6, 7);", SQLConnect))
+                using (SqlCommand Delete = new SqlCommand(
+                    "Delete From ProjectTechnology " +
+                    "Delete From Project " +
+                    "DBCC CHECKIDENT('Project', RESEED, 0) " +
+                    "Declare @UId Nvarchar(450) Set @UId = '7e514a25-c4ab-4214-8414-f9ecc6dd9e9e'  " +
+                    "Delete From AspNetUsers " +
+                    "Insert Into AspNetUsers (Id, UserName, NormalizedUserName, " +
+                    "Email, NormalizedEmail, PasswordHash, SecurityStamp, ConcurrencyStamp, EmailConfirmed,  " +
+                    "PhoneNumberConfirmed, TwoFactorEnabled, LockoutEnabled, AccessFailedCount) Values " +
+                    "(@UId, 'TestUser@a.com','TESTUSER@A.COM', 'TestUser@a.com', 'TESTUSER@A.COM', " +
+                    "'AQAAAAEAACcQAAAAEMjfV0AsErgwEJqu3B/qoRZupd0gee/MR+kTVqHSiw+hyQRqVkwqckRqnX8KWszcAQ==', " +
+                    "'22LCTUSUQEQ3TRILLNAI4UYKWW35DMU4', '9daa59f7-3e7d-43fa-a162-0b10d51fac57', 0, 0, 0, 1, 0)"
+                    +
+                    "Insert Into Project(Title, Description, OwnerId) " +
+                    "Values('FirstProject', 'Our first project will be SUPRISE Hello World', @UId)," +
+                    "('SecondProject', 'Yes we will print Hello Kity', @UId)," +
+                    "('Funny bunny', 'We want to create web aplication with many funny bunes', @UId)," +
+                    "(N'Polish title like pamięć', N'Test for polish worlds like pamięć', @UId)," +
+                    "('Test for adding Project with four Tech', 'Test for adding Project with four Technologies', @UId)," +
+                    "('Project with Two Tech', 'Project with Two Tech (Test)', @UId) "
+                    +
+                    "Insert Into ProjectTechnology Values(5,2),(5, 4),(5, 6),(5, 7),(6, 6),(6, 7);"
+                    , SQLConnect))
                 {
                     Delete.ExecuteNonQuery();
                 }
